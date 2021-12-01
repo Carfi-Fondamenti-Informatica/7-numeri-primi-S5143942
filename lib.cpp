86c25de34a3cbcd29b51954d5913e49b30ec7afd
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-01 12:43:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// created by S5143942 
+//
 
+#include "lib.h"
+bool numerip (int n, int a){
+     if (n<0){
+       return false ;
+     }else if (n==1){
+       return true;
+     }else if (a==1){
+       return true;
+     }else if (n%a==0){
+       return false;
+     }
+  numerip(n,--a);
+}
